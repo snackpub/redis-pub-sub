5087600978875d41e676bd05b4c8602353fba02a
snackpub <1498851306@qq.com>
2020-06-16 21:29:18 +0800

    提交遗漏文件

---
+++ core-coupon-expired/src/main/java/com/snackpub/core/CouponExpiredApplication.java
@@ -0,0 +1,18 @@
+package com.snackpub.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author snack
+ */
+@SpringBootApplication
+public class CouponExpiredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponExpiredApplication.class, args);
+    }
+
+}
